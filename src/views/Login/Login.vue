c7c7e02b62dedc779f59a58a6f59f84357f74d43
russell <311@qq.com>
2020-08-15 17:28:21 +0800

    构建项目、配置全局样式、路由、element-ui

---
+++ src/views/Login/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="login">登录页面</div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+};
+</script>
+
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
